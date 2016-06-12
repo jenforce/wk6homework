a05929c3f0da5dcdaebcfe9ba2b8deb59499e047
jenward <jenforce@aol.com>
2016-06-12 12:14:04 -0400

    update button array, gify call, and button gify call working

--- javascript/giphy.js
+++ javascript/giphy.js
@@ -1,17 +1,21 @@
  $(document).ready(function() {
 var gifThings=[];
-var arrayThings = [];
+var arrayThings = ['bob','cat','waterfall'];
 var thing;
-  
- $(".bthing").on('click', function() {
-         thing = $('#thing-input').val();
-          arrayThings.push(thing);
-          gifCall(thing);
-          renderButtons();
-                    console.log(arrayThings.length);    
-                    console.log(thing);
-    });
 
+ $(".bthing").on('click', function() {
+ 	thing = this.id
+ 	
+	 	if(thing === "thingSubmit"){
+	 		thing = $('#thing-input').val();	
+	 		gifCall(thing);
+         	renderButtons();
+	 	}else{
+         	gifCall(thing);
+	 		console.log(thing);
+	 	}
+ });
+ 	
 function gifCall(thing){
 
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + thing + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -26,33 +30,37 @@ function gifCall(thing){
                              var gifDiv = $('<div class="bthing">');
                              var rating = results[i].rating;
                              var p = $('<p>').text( "Rating: " + rating);
-                             var personImage = $('<img>');
+                             var thingImage = $('<img>');
                              var gifId = results[i].id;
-                            gifThings.push(gifId);
-                              console.log(gifId); 
-//personImage.attr('src', results[i].images.fixed_height.url);
-                            personImage.attr('src', results[i].images.original_still.url);               
+                                 gifThings.push(gifId);
+                                console.log(gifId); 
+                                thingImage.attr('src', results[i].images.original_still.url);               
                             gifDiv.append(p)
-                            gifDiv.append(personImage) 
-                            $('#gifsAppearHere').prepend(gifDiv);    
-        
+                            gifDiv.append(thingImage) 
+                            $('#thingsGoHere').prepend(gifDiv);    
                           }
                         }
-            }); // .done(function(response) {
-    //return false;
-}
+            }); // end .done(function(response)
+}// end function gifCall(thing)
+
 
     function renderButtons(){ 
         $('#buttonCollection').empty();
         for (var i = 0; i < arrayThings.length; i++){
             var a = $('<button>') 
-            a.addClass('bthing');
+            a.addClass("bthing");
+			a.attr('id',arrayThings[i]);
             a.attr('data-name', arrayThings[i]); 
             a.text(arrayThings[i]); 
             $('#buttonCollection').append(a); 
         }
     }
-});    // $(document).ready(function()
 
+});   //end $(document).ready(function()
 
+/*
+
+ //personImage.attr('src', results[i].images.fixed_height.url);
+
+*/
 
